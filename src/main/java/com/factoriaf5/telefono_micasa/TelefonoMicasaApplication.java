a415885e212b441d1d30ccc0a4d6947b1984a650
Hugo Perez Santos <hugopsantos@hotmail.com>
2024-09-11 09:35:56 +0200

    init exercise

---
+++ src/main/java/com/factoriaf5/telefono_micasa/TelefonoMicasaApplication.java
@@ -0,0 +1,13 @@
+package com.factoriaf5.telefono_micasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelefonoMicasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelefonoMicasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/factoriaf5/telefono_micasa/TelefonoMicasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factoriaf5.telefono_micasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelefonoMicasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
