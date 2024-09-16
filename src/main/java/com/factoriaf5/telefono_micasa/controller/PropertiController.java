d8ab5885b3c5cb371b52180ec3663043789bf107
Antonella <168837149+AntonellaEL@users.noreply.github.com>
2024-09-16 15:24:56 +0200

    add models

--- src/main/java/com/factoriaf5/telefono_micasa/controller/PropertiController.java
+++ src/main/java/com/factoriaf5/telefono_micasa/controller/PropertiController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.factoriaf5.telefono_micasa.models.Properti;
 import com.factoriaf5.telefono_micasa.services.PropertiService;
 
 @RestController
@@ -18,7 +19,7 @@ public class PropertiController {
 
 
     @GetMapping("/action_type")
-    public List <Properti> getPropertiesByAction(@RequestParam String type){
+    public List <Properti> getPropertiesByAction(@RequestParam String action){
         return propertiService.getPropertiesByAction(action);
     }
 
--- src/main/java/com/factoriaf5/telefono_micasa/services/PropertiService.java
+++ src/main/java/com/factoriaf5/telefono_micasa/services/PropertiService.java
@@ -1,9 +1,9 @@
 package com.factoriaf5.telefono_micasa.services;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.factoriaf5.telefono_micasa.models.Properti;
 import com.factoriaf5.telefono_micasa.repositories.PropertiRepository;
 
 @Service
