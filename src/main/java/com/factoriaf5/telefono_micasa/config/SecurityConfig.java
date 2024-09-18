ec9b3be675e9cab4d3ae5530e2eecf2e3cbe58d9
Hugo Perez Santos <hugopsantos@hotmail.com>
2024-09-18 11:53:36 +0200

    EncoderFacade done

--- src/main/java/com/factoriaf5/telefono_micasa/config/SecurityConfig.java
+++ src/main/java/com/factoriaf5/telefono_micasa/config/SecurityConfig.java
@@ -5,7 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+/* import org.springframework.security.crypto.password.NoOpPasswordEncoder; */
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -13,69 +14,82 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.http.HttpMethod;
-
-
 import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Value;
 import com.factoriaf5.telefono_micasa.services.JpaUserDetailsService;
+
+import facades.encryptations.Base64Encoder;
 
 @Configuration
 @EnableWebSecurity
 
 public class SecurityConfig {
-    
-    @Value("${api-endpoint}") //ya se refiere al endpoint /api/v1, no es necesario cambiarlo. 
+
+    @Value("${api-endpoint}") // ya se refiere al endpoint /api/v1, no es necesario cambiarlo.
     String endpoint;
 
     MyBasicAuthenticationEntryPoint myBasicAuthenticationEntryPoint;
-    
+
     JpaUserDetailsService jpaUserDetailsService;
 
-    
-    public SecurityConfig(JpaUserDetailsService jpaUserDetailsService, MyBasicAuthenticationEntryPoint basicEntryPoint) {
+    public SecurityConfig(JpaUserDetailsService jpaUserDetailsService,
+            MyBasicAuthenticationEntryPoint basicEntryPoint) {
         this.jpaUserDetailsService = jpaUserDetailsService;
         this.myBasicAuthenticationEntryPoint = basicEntryPoint;
 
-}
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-        .cors(cors -> cors.configurationSource(corsConfiguration()))
-        .csrf(csrf -> csrf.disable())
-        .formLogin(form -> form.disable())
+                .cors(cors -> cors.configurationSource(corsConfiguration()))
+                .csrf(csrf -> csrf.disable())
+                .formLogin(form -> form.disable())
 
-        .authorizeHttpRequests(auth -> auth
-            .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
-            .requestMatchers(HttpMethod.GET, endpoint + "/login").hasAnyRole("ADMIN")
-            .anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
+                        .requestMatchers(HttpMethod.GET, endpoint + "/login").hasAnyRole("ADMIN")
+                        .anyRequest().authenticated())
 
-        .userDetailsService(jpaUserDetailsService)
-        .httpBasic(basic-> basic.authenticationEntryPoint(myBasicAuthenticationEntryPoint))
-        .sessionManagement(session -> session
-        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
+                .userDetailsService(jpaUserDetailsService)
+                .httpBasic(basic -> basic.authenticationEntryPoint(myBasicAuthenticationEntryPoint))
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
 
         http.headers(header -> header.frameOptions(frame -> frame.sameOrigin()));
 
         return http.build();
     }
 
-      @Bean
-        CorsConfigurationSource corsConfiguration() {
-                CorsConfiguration configuration = new CorsConfiguration();
-                configuration.setAllowCredentials(true);
-                configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
-                configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
-                configuration.setAllowedHeaders(Arrays.asList("Authorization"));
+    @Bean
+    CorsConfigurationSource corsConfiguration() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization"));
 
-                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-                source.registerCorsConfiguration("/**", configuration);
-                return source;
-        }
-
-        @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance(); // Desactiva el encriptado
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    Base64Encoder base64Encoder() {
+        return new Base64Encoder();
+    }
+
+    /*
+     * @Bean
+     * public PasswordEncoder passwordEncoder() {
+     * return NoOpPasswordEncoder.getInstance(); // Desactiva el encriptado
+     * }
+     */
+
 }
---
+++ src/main/java/facades/EncoderFacade.java
@@ -0,0 +1,40 @@
+package facades;
+
+
+
+import facades.encryptations.Base64Encoder;
+import com.factoriaf5.telefono_micasa.implementations.IEncryptFacade;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EncoderFacade implements IEncryptFacade {
+
+    PasswordEncoder bCryptPasswordEncoder;
+    Base64Encoder base64Encoder;
+
+    public EncoderFacade(PasswordEncoder bCryptPasswordEncoder, Base64Encoder base64Encoder) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.base64Encoder = base64Encoder;
+    }
+
+    @Override
+    public String encode(String type, String data) {
+        String dataEncrypted = "";
+
+        if (type == "bcrypt") dataEncrypted = bCryptPasswordEncoder.encode(data);
+        if (type == "base64") dataEncrypted = base64Encoder.encode(data);
+
+        return dataEncrypted;
+    }
+
+    @Override
+    public String decode(String type, String data) {
+        String dataDecoded = "";
+
+        if (type == "base64") dataDecoded = base64Encoder.decode(data);
+
+        return dataDecoded;
+    }
+
+}
