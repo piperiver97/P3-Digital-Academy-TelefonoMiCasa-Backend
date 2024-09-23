fc2dd887dcf2a42c7c458b2dfe269659c95f5e04
Antonella <168837149+AntonellaEL@users.noreply.github.com>
2024-09-23 12:42:19 +0200

    property update

--- src/main/java/com/factoriaf5/telefono_micasa/models/Property.java
+++ src/main/java/com/factoriaf5/telefono_micasa/models/Property.java
@@ -1,11 +1,17 @@
 package com.factoriaf5.telefono_micasa.models;
 
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "property_type", discriminatorType = DiscriminatorType.STRING)
 public class Property {
 
     @Id
