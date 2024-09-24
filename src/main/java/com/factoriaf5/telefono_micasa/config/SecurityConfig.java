c17f31b3389494a0e6b3a6e3a325632ab19742f9
Hugo Perez Santos <hugopsantos@hotmail.com>
2024-09-24 09:27:18 +0200

    allowed headers to all, added * on line 75

--- src/main/java/com/factoriaf5/telefono_micasa/config/SecurityConfig.java
+++ src/main/java/com/factoriaf5/telefono_micasa/config/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig {
         configuration.setAllowCredentials(true);
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
